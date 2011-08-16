f237320c44e68bb3f75569f0a8b116694e6bbd87
yukung <yukung.i@gmail.com>
2011-08-16 17:07:12 +0900

    エラーメッセージの出力

--- sample21-14.sh
+++ sample21-14.sh
@@ -5,16 +5,6 @@ then
 	echo 'error:引数は1個でなければなりません' 1>&2
 	exit 1
 fi
-if `echo $1 | grep '[^0-9]' 1>/dev/null`
-then
-	echo 'error:引数は整数でなければなりません' 1>&2
-	exit 1
-fi
-if test $1 -lt 1
-then
-	echo 'error:引数は1以上である必要があります' 1>&2
-	exit 1
-fi
 for i in `seq 1 $1`
 do
 	SUM=`expr $SUM + $i`
