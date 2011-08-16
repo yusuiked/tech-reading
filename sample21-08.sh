38ca20d8cd17dd7486107304224f83bd5f903f48
yukung <yukung.i@gmail.com>
2011-08-16 15:43:15 +0900

    評価結果を文字列で出力（elif～then版）

--- sample21-08.sh
+++ sample21-08.sh
@@ -7,8 +7,9 @@ read VAR2
 if test $VAR1 -eq $VAR2
 then
 	echo 'equal'
+elif test $VAR1 -gt $VAR2
+then
+	echo 'VAR1 is greater than VAR2. ($VAR1 > $VAR2)'
 else
-	echo 'not equal'
+	echo 'VAR1 is less than VAR2. ($VAR1 < $VAR2)'
 fi
-
-if test $VAR1 -eq $VAR2 ; then echo 'equal' ; else echo 'not equal' ; fi ;
