26799ddbd930d099eb60d1bd7d4488da2a39895f
yukung <yukung.i@gmail.com>
2011-08-18 19:34:34 +0900

    コマンド列が異常終了してもエラーメッセージを出力しないように、標準エラー出力も /dev/null に捨てるように変更

--- sample22_functions.sh
+++ sample22_functions.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 my_func_check_integer() {
-	if `echo $1 | grep '[^0-9]' 1>/dev/null`
+	if `echo $1 | grep '[^0-9]' 1>/dev/null 2>&1`
 	then
 		echo 'error: 引数は整数でなければなりません' 1>&2
 		exit 1
