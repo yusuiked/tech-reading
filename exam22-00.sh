adc45bfe3f54fbd7bcac810e9d5a803349b57e41
yukung <yukung.i@gmail.com>
2011-08-21 18:52:13 +0900

    .bak拡張子の重複を考慮する（その２）

--- exam22-00.sh
+++ exam22-00.sh
@@ -2,13 +2,15 @@
 
 readonly SUFFIX='.bak'
 
-FILELIST=`find . -maxdepth 1 -type f | grep -v '\.bak$'`
-
-if [ $? -ne 0 ]; then exit 1; fi
+FILELIST=`find . -maxdepth 1 -type f`
 
 for fname_from in $FILELIST
 do
-	fname_to="$fname_from.bak"
-	cp $fname_from $fname_to
+	if echo $fname_from | grep -v '\.bak$' 1>/dev/null 2>&1
+	then
+		fname_to="$fname_from$SUFFIX"
+		cp $fname_from $fname_to
+	fi
+	if [ $? -ne 0 ]; then exit 1; fi
 done	
 exit 0
