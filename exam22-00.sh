58047c69a6d4945c6b2a448de367dbdb11d884b4
yukung <yukung.i@gmail.com>
2011-08-21 18:48:54 +0900

    .bak拡張子の重複を考慮する

---
+++ exam22-00.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+readonly SUFFIX='.bak'
+
+FILELIST=`find . -maxdepth 1 -type f | grep -v '\.bak$'`
+
+if [ $? -ne 0 ]; then exit 1; fi
+
+for fname_from in $FILELIST
+do
+	fname_to="$fname_from.bak"
+	cp $fname_from $fname_to
+done	
+exit 0
