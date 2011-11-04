d9674cc23abd672c58c3ab7a9230fb41aae867e6
yukung <yukung.i@gmail.com>
2011-11-04 18:42:14 +0900

    XMLを一部加工して出力する（ストリーミング処理）

---
+++ StreamingChangeTextNode.groovy
@@ -0,0 +1,35 @@
+import groovy.xml.StreamingMarkupBuilder
+
+def root = new XmlSlurper().parse('sample.xml')
+
+def groceries = root.category.findAll { it.@type == 'groceries' }
+def g = groceries[0]
+
+g.item.eachWithIndex { item, i ->
+    g.item[i] = 'luxury ' + item
+}
+
+def supplies = root.category.findAll {it.@type == 'supplies' }
+def pens = supplies[0].item.findAll { it.text() == 'pen' }
+pens.each { p ->
+    p.@quantity = (p.@quantity.toInteger() + 2).toString()
+    p.@when = 'Urgent'
+}
+
+def presents = root.category.find { it.@type == 'present' }
+presents.replaceNode { node ->
+    category(type:'present') {
+        item("mother's birthday")
+        item("Monica's birthday", when:'Oct 15')
+    }
+}
+
+def out = new OutputStreamWriter(
+    new FileOutputStream("xmlslurper.output.xml"),"UTF-8")
+
+def outputBuilder = new StreamingMarkupBuilder()
+def writer = outputBuilder.bind {
+    mkp.xmlDeclaration()
+    mkp.yield(root)
+}
+out << writer
