7efc62ac226c45e0c7ff2b6e14789b56b3069b5f
yukung <yukung.i@gmail.com>
2011-11-04 14:20:05 +0900

    withを使った書き換え

--- CliBuilderSample.groovy
+++ CliBuilderSample.groovy
@@ -1,8 +1,17 @@
 def cli = new CliBuilder(usage:'groovy CliBuilderSample.groovy -f filename [-d]')
 
-cli.h(longOpt:'help', 'ヘルプ')
-cli.f(longOpt:'file', required:true, args:1, '処理対象ファイル名')
-cli.d(longOpt:'debug', 'デバッグモード')
+// cli.h(longOpt:'help', 'ヘルプ')
+// cli.f(longOpt:'file', required:true, args:1, '処理対象ファイル名')
+// cli.d(longOpt:'debug', 'デバッグモード')
+
+/*
+ * withを使った書き換え
+ */
+cli.with {
+    h longOpt:'help', 'ヘルプ'
+    f longOpt:'file', required:true, args:1, '処理対象ファイル名'
+    d longOpt:'debug', 'デバッグモード'
+}
 
 def options = cli.parse(args)
 
