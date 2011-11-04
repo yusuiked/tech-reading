11be50068cc2cf0e5fa6a6295db952fc53c86d84
yukung <yukung.i@gmail.com>
2011-11-04 17:42:57 +0900

    処理内容のコメント追加

--- StreamingPrintXml.groovy
+++ StreamingPrintXml.groovy
@@ -1,13 +1,16 @@
 import groovy.xml.StreamingMarkupBuilder
 
-def root = new XmlSlurper().parse('sample.xml')
+def root = new XmlSlurper().parse('sample.xml') // GPathResultオブジェクトが得られるので，StreamingMarkupBuilderにそのままバインド出来る
 
 def out = new OutputStreamWriter(
     new FileOutputStream("xmlslurper.output.xml"), "UTF-8")
 
 def outputBuilder = new StreamingMarkupBuilder()
 def writer = outputBuilder.bind {
-    mkp.xmlDeclaration()
-    mkp.yield(root)
+    /*
+     * mkpはGroovyが事前に予約している特殊な名前空間として認識される
+     */
+    mkp.xmlDeclaration()    // XML宣言の出力
+    mkp.yield(root) // 事前に定義したクロージャを，ビルダーのメソッド呼び出しに読み替える
 }
 out << writer
