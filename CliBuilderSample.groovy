d6800ea397dd34eeca0486d6dc15bd099c5d8e3a
yukung <yukung.i@gmail.com>
2011-11-04 14:18:00 +0900

    CliBuilderの使い方

---
+++ CliBuilderSample.groovy
@@ -0,0 +1,18 @@
+def cli = new CliBuilder(usage:'groovy CliBuilderSample.groovy -f filename [-d]')
+
+cli.h(longOpt:'help', 'ヘルプ')
+cli.f(longOpt:'file', required:true, args:1, '処理対象ファイル名')
+cli.d(longOpt:'debug', 'デバッグモード')
+
+def options = cli.parse(args)
+
+if (!options) return
+
+if (options.h) {
+    cli.usage()
+    return
+}
+
+if (options.f) println "オプション-fが指定されました（引数=${options.f})"
+
+if (options.d) println "オプション-dが指定されました"
