4b6ff5af063fd1c51065d9a73638048cf793bfa0
yukung <yukung.i@gmail.com>
2012-01-06 17:15:57 +0000

    Fixed StringBuilder Sample.
    git-svn-id: https://subversion.assembla.com/svn/tech-reading/perfect-java/trunk@16 ebad469c-0468-4bce-90df-43cd89fd903f

--- src/main/java/org/yukung/perfect_java/chap2/StringBuilderTest.java
+++ src/main/java/org/yukung/perfect_java/chap2/StringBuilderTest.java
@@ -5,10 +5,25 @@ public class StringBuilderTest {
 	public static void main(String[] args) {
 		StringBuilder sb = new StringBuilder("0123456789");
 		char charAt = sb.charAt(3);
+		System.out.println(charAt);
 		int length = sb.length();
+		System.out.println(length);
 		sb.append(8);
 		System.out.println(sb.toString());
-		
+		sb.delete(1, 2);
+		System.out.println(sb.toString());
+		int indexOf = sb.indexOf("6");
+		System.out.println(indexOf);
+		sb.insert(8, 'f');
+		System.out.println(sb.toString());
+		sb.replace(0, 5, "543210");
+		System.out.println(sb.toString());
+		sb.setCharAt(0, '7');
+		System.out.println(sb.toString());
+		String substring = sb.substring(5);
+		System.out.println(substring);
+		sb.reverse();
+		System.out.println(sb.toString());
 	}
 	
 }
