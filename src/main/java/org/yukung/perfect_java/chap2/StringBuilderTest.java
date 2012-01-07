2652a5b006061b6bac92dbd07df2d45459164e9e
yukung <yukung.i@gmail.com>
2012-01-07 03:47:22 +0000

    文字列結合 += と StringBuilder#append() の実行速度の違いを計測。 Refactor StringBuilderTest.
    git-svn-id: https://subversion.assembla.com/svn/tech-reading/perfect-java/trunk@19 ebad469c-0468-4bce-90df-43cd89fd903f

--- src/main/java/org/yukung/perfect_java/chap2/StringBuilderTest.java
+++ src/main/java/org/yukung/perfect_java/chap2/StringBuilderTest.java
@@ -1,8 +1,47 @@
 package org.yukung.perfect_java.chap2;
 
+import org.apache.commons.lang3.time.StopWatch;
+
 public class StringBuilderTest {
 	
+	/*
+	 * 無駄が多い文字列結合。
+	 *
+	 * += の結合時にStrigBuilderが都度生成され、さらにappend()が2度呼ばれる。
+	 * sのStringオブジェクトをappend,resultのStringオブジェクトをappend。
+	 *
+	 * time: 64093ms
+	 */
+	public static String concat(String[] array) {
+		String result = "";
+		for (String s : array) {
+			result += s;
+		}
+		return result;
+	}
+	
+	/*
+	 * StringBuilder#append()を使った例。
+	 * StringBuilderが一度だけ生成され、append()も一度しか呼ばれない。
+	 *
+	 * time: 9ms
+	 */
+	public static String effectiveConcat(String[] array) {
+		StringBuilder result = new StringBuilder();
+		for (String s : array) {
+			result.append(s);
+		}
+		return result.toString();
+	}
+	
 	public static void main(String[] args) {
+		lookAtBehavior();
+		
+		measureConcatenate();
+		
+	}
+	
+	private static void lookAtBehavior() {
 		StringBuilder sb = new StringBuilder("0123456789");
 		char charAt = sb.charAt(3);
 		System.out.println(charAt);
@@ -26,4 +65,26 @@ public class StringBuilderTest {
 		System.out.println(sb.toString());
 	}
 	
+	private static void measureConcatenate() {
+		String[] data = new String[100000];
+		for (int i = 0; i < 100000; i++) {
+			data[i] = Integer.toString(i);
+		}
+		
+		long start = System.currentTimeMillis();
+		
+		concat(data);
+		
+		long end = System.currentTimeMillis();
+		System.out.printf("time: %sms%n", end - start);
+		
+		StopWatch stopWatch = new StopWatch();
+		stopWatch.start();
+		
+		effectiveConcat(data);
+		
+		stopWatch.stop();
+		
+		System.out.printf("time: %sms%n", stopWatch.getTime());
+	}
 }
