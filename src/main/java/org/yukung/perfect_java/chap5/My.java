202fdda6c2b0a432071e5fbcba04ef3a5ec4ecc2
yukung <yukung.i@gmail.com>
2012-01-28 11:14:30 +0000

    フィールド変数の初期化のタイミング。
    git-svn-id: https://subversion.assembla.com/svn/tech-reading/perfect-java/trunk@53 ebad469c-0468-4bce-90df-43cd89fd903f

--- src/main/java/org/yukung/perfect_java/chap5/My.java
+++ src/main/java/org/yukung/perfect_java/chap5/My.java
@@ -5,10 +5,14 @@ package org.yukung.perfect_java.chap5;
  */
 public class My {
 	
-	public static void main(String[] args) {
-		My my = new My();
+	public static void main(String[] args) throws Exception {
+		My my1 = new My();
+		System.out.println(my1.createdAt);
+		Thread.sleep(1000);
+		My my2 = new My();
+		System.out.println(my2.createdAt);
 	}
 	
 	
-	My my = new My();
+	long createdAt = System.currentTimeMillis(); // オブジェクト生成時に初期化される
 }
