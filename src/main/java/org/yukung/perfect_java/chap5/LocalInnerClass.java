8d5f49605f80955fc24524f03506f906cfbda1f4
yukung <yukung.i@gmail.com>
2012-02-12 10:45:02 +0000

    ローカル内部クラスを使う例。 refs #18 @0.5h
    git-svn-id: https://subversion.assembla.com/svn/tech-reading/perfect-java/trunk@65 ebad469c-0468-4bce-90df-43cd89fd903f

--- src/main/java/org/yukung/perfect_java/chap5/LocalInnerClass.java
+++ src/main/java/org/yukung/perfect_java/chap5/LocalInnerClass.java
@@ -1,5 +1,7 @@
 package org.yukung.perfect_java.chap5;
 
+import java.util.Comparator;
+
 public class LocalInnerClass {
 	
 	void doit() {
@@ -12,5 +14,20 @@ public class LocalInnerClass {
 		
 		LocalClass lc = new LocalClass();
 		lc.doit();
+	}
+	
+	/*
+	 * ローカル内部クラスを使う目的でよく使われる例。
+	 */
+	Comparator<String> getComparator() {
+		class StringLengthComparator implements Comparator<String> {
+			
+			@Override
+			public int compare(String s1, String s2) {
+				return s1.length() - s2.length();
+			}
+			
+		}
+		return new StringLengthComparator();
 	}
 }
