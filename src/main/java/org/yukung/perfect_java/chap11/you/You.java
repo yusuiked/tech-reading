b1d1d910bee39aee74c96119d2cce12f4c66d1e4
yukung <yukung.i@gmail.com>
2012-07-01 09:04:46 +0000

    パッケージプライベートで実装クラスを隠蔽する例を追加。 refs #24 fixes
    git-svn-id: https://subversion.assembla.com/svn/tech-reading/perfect-java/trunk@75 ebad469c-0468-4bce-90df-43cd89fd903f

--- src/main/java/org/yukung/perfect_java/chap11/you/You.java
+++ src/main/java/org/yukung/perfect_java/chap11/you/You.java
@@ -7,7 +7,7 @@ public class You {
 	
 	public static void main(String[] args) {
 		My my = MyFactory.createMy();
-//		MyImpl myImpl = new MyImpl();	// パッケージプライベートであるため、クラスが解決できない
+//		MyImpl myImpl = new MyImpl();	// パッケージプライベートなので、クラスが解決できない
 		my.doit(); // => "my.MyImpl"を出力
 	}
 	
