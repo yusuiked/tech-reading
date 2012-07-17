fc2b4868d4f928e0e0db20584d7c8d052bbf9a9d
yukung <yukung.i@gmail.com>
2012-07-17 09:46:34 +0000

    独自例外クラスのコンストラクタ定義の例。 refs #26 @1.0h
    git-svn-id: https://subversion.assembla.com/svn/tech-reading/perfect-java/trunk@79 ebad469c-0468-4bce-90df-43cd89fd903f

---
+++ src/main/java/org/yukung/perfect_java/chap13/MyRuntimeException.java
@@ -0,0 +1,12 @@
+package org.yukung.perfect_java.chap13;
+
+public class MyRuntimeException extends RuntimeException {
+	
+	public MyRuntimeException() {
+		super();
+	}
+	
+	public MyRuntimeException(String message) {
+		super(message);
+	}
+}
