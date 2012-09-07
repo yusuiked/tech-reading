ee2c380d34bb5b8188350f894d53fde3aa55ef73
yukung <yukung.i@gmail.com>
2012-09-07 07:41:21 +0000

    ディレクトリのファイル一覧を出力する例。 refs #28
    git-svn-id: https://subversion.assembla.com/svn/tech-reading/perfect-java/trunk@86 ebad469c-0468-4bce-90df-43cd89fd903f

--- src/main/java/org/yukung/perfect_java/chap15/DirectoryOperation.java
+++ src/main/java/org/yukung/perfect_java/chap15/DirectoryOperation.java
@@ -5,14 +5,21 @@ import java.io.File;
 public class DirectoryOperation {
 	
 	public static void main(String[] args) {
-		File file = new File("./src/main/java/org/yukung/perfect_java/chap15/hoge");
-		file.mkdir();
-		if (file.exists()) {
-			file.renameTo(new File("./src/main/java/org/yukung/perfect_java/chap15/fuga"));
+		File dir = new File("./src/main/java/org/yukung/perfect_java/chap15/hoge");
+		dir.mkdir();
+		if (dir.exists()) {
+			dir.renameTo(new File("./src/main/java/org/yukung/perfect_java/chap15/fuga"));
 		}
-		file =
+		dir =
 				new File(
 						"/Users/yukung/Documents/workspace/perfect_java/src/main/java/org/yukung/perfect_java/chap15/fuga");
-		file.delete();
+		dir.delete();
+		
+		String path = "."; // カレントディレクトリ
+		File dir2 = new File(path);
+		String[] files = dir2.list();
+		for (String file : files) {
+			System.out.println(file);
+		}
 	}
 }
