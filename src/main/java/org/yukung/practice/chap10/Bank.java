3378b5a892adb289b099e8d29167b3a5ddd08617
yukung <yukung.i@gmail.com>
2012-11-15 22:16:45 +0900

    Add sample class for documentation comment.

---
+++ src/main/java/org/yukung/practice/chap10/Bank.java
@@ -0,0 +1,10 @@
+package org.yukung.practice.chap10;
+
+public class Bank {
+	String name;
+	String address;
+	public void addAccount(String owner, int initialBalance) {}
+	public static void main(String[] args) {
+		System.out.println("試験用のメインメソッドです");
+	}
+}
