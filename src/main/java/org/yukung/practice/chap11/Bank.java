071185db98e3bc7310186e029aadb41e92f6da60
yukung <yukung.i@gmail.com>
2012-11-27 22:46:17 +0900

    Fix bug.

--- src/main/java/org/yukung/practice/chap11/Bank.java
+++ src/main/java/org/yukung/practice/chap11/Bank.java
@@ -8,6 +8,9 @@ public class Bank {
 	}
 
 	public void setName(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("名前が設定されていません");
+		}
 		if (name.length() < 3) {
 			throw new IllegalArgumentException("名前が不正です");
 		}
