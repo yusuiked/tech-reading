94a62c612c0c45d45824d9fb5b70e6ad9be7c7c8
yukung <yukung.i@gmail.com>
2013-04-11 10:48:09 +0900

    change version of 'commons-lang'.

--- practical-java-tutorial-clearly/src/main/java/org/yukung/practice/chap5/Book.java
+++ practical-java-tutorial-clearly/src/main/java/org/yukung/practice/chap5/Book.java
@@ -2,60 +2,60 @@ package org.yukung.practice.chap5;
 
 import java.util.Date;
 
-import org.apache.commons.lang3.builder.CompareToBuilder;
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang.builder.CompareToBuilder;
+import org.apache.commons.lang.builder.EqualsBuilder;
+import org.apache.commons.lang.builder.HashCodeBuilder;
 
 public class Book implements Comparable<Book>, Cloneable {
-	private String title;
-	private Date publishDate;
-	private String comment;
+    private String title;
+    private Date publishDate;
+    private String comment;
 
-	public String getTitle() {
-		return title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public void setTitle(String title) {
-		this.title = title;
-	}
+    public void setTitle(String title) {
+        this.title = title;
+    }
 
-	public Date getPublishDate() {
-		return publishDate;
-	}
+    public Date getPublishDate() {
+        return publishDate;
+    }
 
-	public void setPublishDate(Date publishDate) {
-		this.publishDate = publishDate;
-	}
+    public void setPublishDate(Date publishDate) {
+        this.publishDate = publishDate;
+    }
 
-	public String getComment() {
-		return comment;
-	}
+    public String getComment() {
+        return comment;
+    }
 
-	public void setComment(String comment) {
-		this.comment = comment;
-	}
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		return EqualsBuilder.reflectionEquals(this, obj);
-	}
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
 
-	@Override
-	public int hashCode() {
-		return HashCodeBuilder.reflectionHashCode(this);
-	}
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
 
-	@Override
-	public int compareTo(Book o) {
-		return CompareToBuilder.reflectionCompare(this, o);
-	}
+    @Override
+    public int compareTo(Book o) {
+        return CompareToBuilder.reflectionCompare(this, o);
+    }
 
-	@Override
-	public Book clone() {
-		Book b = new Book();
-		b.title = this.title;
-		b.comment = this.comment;
-		b.publishDate = (Date) this.publishDate.clone();
-		return b;
-	}
+    @Override
+    public Book clone() {
+        Book b = new Book();
+        b.title = this.title;
+        b.comment = this.comment;
+        b.publishDate = (Date) this.publishDate.clone();
+        return b;
+    }
 }
