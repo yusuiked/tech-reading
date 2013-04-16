69170b63330c1996c2b292d30b1cf32729cfc796
yukung <yukung.i@gmail.com>
2013-04-16 15:11:26 +0900

    Fix bug.

--- practical-java-tutorial-clearly/src/test/java/org/yukung/practice/chap8/DownloaderTest.java
+++ practical-java-tutorial-clearly/src/test/java/org/yukung/practice/chap8/DownloaderTest.java
@@ -11,27 +11,26 @@ import org.junit.Test;
 
 public class DownloaderTest {
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Before
+    public void setUp() throws Exception {
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @After
+    public void tearDown() throws Exception {
+    }
 
-	@Test
-	public void testDownload() throws Exception {
-		Downloader downloader = new Downloader("http://dokojava.jp/favicon.ico");
-		downloader.download("dj.ico");
+    @Test
+    public void testDownload() throws Exception {
+        Downloader downloader = new Downloader("http://dokojava.jp/favicon.ico");
+        downloader.download("dj.ico");
 
-		File file = new File("dj.ico");
-		assertThat(file,is(notNullValue()));
-		assertThat(file.exists(),is(true));
-		assertThat(file.isFile(),is(true));
-		assertThat(file.getAbsolutePath(),is("/Users/yukung/Documents/tech-reading/practical-java-tutorial-clearly/dj.ico"));
-		assertThat(file.getCanonicalPath(),is("/Users/yukung/Documents/tech-reading/practical-java-tutorial-clearly/dj.ico"));
-		assertThat(file.getName(),is("dj.ico"));
-		assertThat(file.length(),is(22638L));
-	}
+        File file = new File("dj.ico");
+        assertThat(file,is(notNullValue()));
+        assertThat(file.exists(),is(true));
+        assertThat(file.isFile(),is(true));
+        assertThat(file.getPath(),is("dj.ico"));
+        assertThat(file.getName(),is("dj.ico"));
+        assertThat(file.length(),is(22638L));
+    }
 
 }
