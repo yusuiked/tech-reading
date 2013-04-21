12b7aa2d00d6854455e8336bd47cb323eff9229c
yukung <yukung.i@gmail.com>
2013-04-21 19:08:02 +0900

    スレッドを利用するように変更。

--- practical-java-tutorial-clearly/src/main/java/org/yukung/practice/chap16/ThreadSample.java
+++ practical-java-tutorial-clearly/src/main/java/org/yukung/practice/chap16/ThreadSample.java
@@ -5,9 +5,18 @@ import java.util.Scanner;
 public class ThreadSample {
 	public static void main(String[] args) {
 		System.out.println("何か入力してください");
+		Thread t = new PrintingThread();
+		t.start();
 		new Scanner(System.in).nextLine();
+	}
+}
+
+class PrintingThread extends Thread {
+	@Override
+	public void run() {
 		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
+
 	}
 }
