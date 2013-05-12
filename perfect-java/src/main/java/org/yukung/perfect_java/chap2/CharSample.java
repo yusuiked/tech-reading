d6313649352748fa9fbd793d45c685f6c742b8b2
yukung <yukung.i@gmail.com>
2013-05-12 15:27:52 +0900

    Add sample of character literal.

---
+++ perfect-java/src/main/java/org/yukung/perfect_java/chap2/CharSample.java
@@ -0,0 +1,13 @@
+package org.yukung.perfect_java.chap2;
+
+public class CharSample {
+	
+	public static void main(String[] args) {
+		char c = 97;
+		System.out.println(c);
+		char i = 0x61;
+		System.out.println(i);
+		char u = '\u0061';
+		System.out.println(u);
+	}
+}
