b5a4775a10b63175927d35a0606588d8cb05abd1
yukung <yukung.i@gmail.com>
2013-08-29 11:44:57 +0900

    Add action for detail page.

---
+++ webapp-development-with-jboss-seam/seambook_nomikai/src/hot/action/NomikaiDetailAction.java
@@ -0,0 +1,15 @@
+package action;
+
+import org.jboss.seam.annotations.Name;
+
+@Name("nomikaiDetailAction")
+public class NomikaiDetailAction {
+
+	public void page() {
+
+	}
+
+	public String confirm() {
+		return null;
+	}
+}
