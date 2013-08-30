60bf65492dccdd22e6fd740bbacaaa3bc27069a8
yukung <yukung.i@gmail.com>
2013-08-30 12:28:12 +0900

    Implemented detail page, and detail page action.

--- webapp-development-with-jboss-seam/seambook_nomikai/src/hot/action/NomikaiDetailAction.java
+++ webapp-development-with-jboss-seam/seambook_nomikai/src/hot/action/NomikaiDetailAction.java
@@ -1,16 +1,28 @@
 package action;
 
-import java.util.List;
+import java.util.Set;
 
+import javax.persistence.EntityManager;
+
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Conversational;
 import org.jboss.seam.annotations.In;
+import org.jboss.seam.annotations.Logger;
 import org.jboss.seam.annotations.Name;
 import org.jboss.seam.annotations.Out;
+import org.jboss.seam.annotations.Scope;
 import org.jboss.seam.annotations.datamodel.DataModel;
+import org.jboss.seam.annotations.datamodel.DataModelSelection;
+import org.jboss.seam.contexts.Context;
+import org.jboss.seam.contexts.Contexts;
+import org.jboss.seam.log.Log;
 
 import dto.NomikaiDetailDto;
 import entity.Nomikai;
 import entity.Nomikaimember;
 
+@Conversational
+@Scope(ScopeType.CONVERSATION)
 @Name("nomikaiDetailAction")
 public class NomikaiDetailAction {
 
@@ -21,13 +33,29 @@ public class NomikaiDetailAction {
 	private Nomikai nomikai;
 
 	@DataModel
-	private List<Nomikaimember> nomikaimembers;
+	private Set<Nomikaimember> nomikaimembers;
+
+	@DataModelSelection("nomikaimembers")
+	private Nomikaimember nomikaimember;
+
+	@In
+	private EntityManager entityManager;
+
+	@Logger
+	private Log logger;
 
 	public void page() {
-
+		logger.info("find nomikaiid is #0", nomikaiDetailDto.getNomikaiid());
+		this.nomikai = entityManager.find(Nomikai.class, nomikaiDetailDto
+				.getNomikaiid());
+		this.nomikaimembers = nomikai.getNomikaimemberCollection();
 	}
 
 	public String confirm() {
+		logger.info("selected member is #0", nomikaimember.getUserid()
+				.getName());
+		Context conversationContext = Contexts.getConversationContext();
+		conversationContext.set("nomikaimember", nomikaimember);
 		return "confirm";
 	}
 }
--- webapp-development-with-jboss-seam/seambook_nomikai/src/hot/dto/NomikaiDetailDto.java
+++ webapp-development-with-jboss-seam/seambook_nomikai/src/hot/dto/NomikaiDetailDto.java
@@ -2,6 +2,9 @@ package dto;
 
 import java.io.Serializable;
 
+import org.jboss.seam.annotations.Name;
+
+@Name("nomikaiDetailDto")
 public class NomikaiDetailDto implements Serializable {
 
 	private static final long serialVersionUID = 1L;
