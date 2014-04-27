f931fbcffb1a099995c2be543dae70f475aa6b87
yukung <yukung.i@gmail.com>
2014-04-27 16:30:19 +0900

    Add feature of update and delete.

--- play-framework-thorough-tutorial/play-app/app/controllers/Application.java
+++ play-framework-thorough-tutorial/play-app/app/controllers/Application.java
@@ -32,4 +32,59 @@ public class Application extends Controller {
             return badRequest(add.render("ERROR", f));
         }
     }
+
+    public static Result setitem() {
+        Form<Message> f = new Form(Message.class);
+        return ok(item.render("ID番号を入力。", f));
+    }
+
+    public static Result edit() {
+        Form<Message> f = new Form(Message.class).bindFromRequest();
+        if (!f.hasErrors()) {
+            Message obj = f.get();
+            Long id = obj.id;
+            obj = Message.find.byId(id);
+            if (obj != null) {
+                f = new Form(Message.class).fill(obj);
+                return ok(edit.render("ID=" + id + "の投稿を編集。", f));
+            } else {
+                return ok(item.render("ERROR:IDの投稿が見つかりません。", f));
+            }
+        } else {
+            return ok(item.render("ERROR:入力に問題があります。", f));
+        }
+    }
+
+    public static Result update() {
+        Form<Message> f = new Form(Message.class).bindFromRequest();
+        if (!f.hasErrors()) {
+            Message data = f.get();
+            data.update();
+            return redirect("/");
+        } else {
+            return ok(edit.render("ERROR:再度入力してください。", f));
+        }
+    }
+
+    public static Result delete() {
+        Form<Message> f = new Form(Message.class);
+        return ok(delete.render("削除するID番号", f));
+    }
+
+    public static Result remove() {
+        Form<Message> f = new Form(Message.class).bindFromRequest();
+        if (!f.hasErrors()) {
+            Message obj = f.get();
+            Long id = obj.id;
+            obj = Message.find.byId(id);
+            if (obj != null) {
+                obj.delete();
+                return redirect("/");
+            } else {
+                return ok(delete.render("ERROR:そのID番号は見つかりません。", f));
+            }
+        } else {
+            return ok(delete.render("ERROR:入力にエラーが起こりました。", f));
+        }
+    }
 }
