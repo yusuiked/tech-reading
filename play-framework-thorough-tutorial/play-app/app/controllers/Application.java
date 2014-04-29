631ca0587088e6dd32ec651af6703baebc47ebe2
yukung <yukung.i@gmail.com>
2014-04-29 16:15:08 +0900

    Generate select box with member name.

--- play-framework-thorough-tutorial/play-app/app/controllers/Application.java
+++ play-framework-thorough-tutorial/play-app/app/controllers/Application.java
@@ -11,14 +11,20 @@ import java.util.*;
 
 public class Application extends Controller {
 
-    public static class SampleForm {
-        public List<String> inputs;
-    }
-
     public static Result index() {
         // Form<SampleForm> form = new Form(SampleForm.class);
         List<Message> msgs = Message.find.all();
         return ok(index.render("please set form.", msgs));
+    }
+
+    public static Result add() {
+        Form<Message> f = new Form(Message.class);
+        List<Member> mems = Member.find.select("name").findList();
+        return ok(add.render("投稿フォーム" , f, mems));
+    }
+
+    public static Result create() {
+        return redirect("/");
     }
 
 }
