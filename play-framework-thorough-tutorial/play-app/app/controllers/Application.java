c01d65ae2002aa20f9f3332454e8c029984233ea
yukung <yukung.i@gmail.com>
2014-04-29 14:29:17 +0900

    The changes to the form binging.

--- play-framework-thorough-tutorial/play-app/app/controllers/Application.java
+++ play-framework-thorough-tutorial/play-app/app/controllers/Application.java
@@ -11,14 +11,41 @@ import java.util.*;
 
 public class Application extends Controller {
 
+    public static class SampleForm {
+        public String input;
+        public String pass;
+        public boolean check;
+        public String radio;
+        public String sel;
+        public String area;
+        public Date date;
+    }
+
     public static Result index() {
-        String method = request().method();
-        if ("GET".equals(method)) {
-            return ok(index.render("please type:"));
+        SampleForm sf = new SampleForm();
+        sf.radio = "windows";
+        sf.check = true;
+        sf.input = "default value";
+        sf.sel = "uk";
+        Form<SampleForm> form = new Form(SampleForm.class).fill(sf);
+        return ok(index.render("please set form.", form));
+    }
+
+    public static Result send() {
+        Form<SampleForm> f = new Form(SampleForm.class).bindFromRequest();
+        if (!f.hasErrors()) {
+            SampleForm sf = f.get();
+            String res = "value: ";
+            res += "input=" + sf.input
+                + ", pass=" + sf.pass
+                + ", check=" + sf.check
+                + ", radio=" + sf.radio
+                + ", sel=" + sf.sel
+                + ", area=" + sf.area
+                + ", date=" + sf.date;
+            return ok(index.render(res, f));
         } else {
-            Map<String, String[]> form = request().body().asFormUrlEncoded();
-            String[] input = form.get("input");
-            return ok(index.render("posted:" + input[0]));
+            return badRequest(index.render("ERROR", f));
         }
     }
 
