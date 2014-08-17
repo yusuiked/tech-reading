0bf030d12979c262f18e2551b74e1af9fd623cbd
yukung <yukung.i@gmail.com>
2014-08-17 16:21:24 +0900

    Add the image files and define the variable of photo list in the script.

--- javascript-for-non-programmers/sample.js
+++ javascript-for-non-programmers/sample.js
@@ -3,4 +3,12 @@ window.onload = function() {
 	var photo = document.getElementById('photo');
 	var nextBtn = document.getElementById('nextBtn');
 	var title = document.getElementById('title');
+
+	// 画像のリストの定義
+	var photoList = [
+		{ src: 'img/spring.jpg', title: '春の桜' },
+		{ src: 'img/summer.jpg', title: '夏のひまわり' },
+		{ src: 'img/autumn.jpg', title: '秋の紅葉' },
+		{ src: 'img/winter.jpg', title: '冬の山' }
+	];
 }
