bd0bbe38b63c3e48617336ccbcfa6ed446730ea8
yukung <yukung.i@gmail.com>
2014-08-17 16:12:23 +0900

    Add the JavaScript file.

---
+++ javascript-for-non-programmers/sample.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	// 要素の取得
+	var photo = document.getElementById('photo');
+	var nextBtn = document.getElementById('nextBtn');
+	var title = document.getElementById('title');
+}
