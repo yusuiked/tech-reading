300a5fb69fca6134a8612bf10651483c9771b9af
yukung <yukung.i@gmail.com>
2014-08-30 17:21:44 +0900

    Modify so could call the hello.js from an external function.

--- nodejs-firststep/chap04/hello.js
+++ nodejs-firststep/chap04/hello.js
@@ -1,7 +1,9 @@
 var myName = 'Taro';
+exports.myName = myName;
 
 function greeting(something) {
 	console.log(exports.myName + ': hello, ' + something + '!');
 }
+exports.greeting = greeting;
 
 greeting('world');
