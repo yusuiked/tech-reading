ff3136448421acf362291b223a7daf01dbd762e8
yukung <yukung.i@gmail.com>
2014-11-08 21:31:29 +0900

    AOPの指定をアノテーションからBean定義ファイルに変更

--- spring3-introduction/chap03/src/main/java/sample/aop/aspect/MyFirstAspect.java
+++ spring3-introduction/chap03/src/main/java/sample/aop/aspect/MyFirstAspect.java
@@ -12,11 +12,11 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 import sample.di.business.entity.Product;
 
-@Aspect
-@Component
+//@Aspect
+//@Component
 public class MyFirstAspect {
 
-    @Before("execution(* findProduct())")
+//    @Before("execution(* findProduct())")
     public void before(JoinPoint jp) {
         System.out.println("Hello Before! *** メソッドが呼ばれる前に出てくるよ！");
         Signature sig = jp.getSignature();
@@ -25,18 +25,18 @@ public class MyFirstAspect {
 //        System.out.println("-----> 引数の値を取得するよ：" + objs[0]);
     }
 
-    @After("execution(* findProduct())")
+//    @After("execution(* findProduct())")
     public void after() {
         System.out.println("Hello After! *** メソッドを呼んだ後に出てくるよ！");
     }
 
-    @AfterReturning(value = "execution(* findProduct())", returning = "product")
+//    @AfterReturning(value = "execution(* findProduct())", returning = "product")
     public void afterReturning(Product product) {
         System.out.println("Hello AfterReturning! *** メソッドを呼んだ後に出てくるよ！");
         System.out.println("-----> 戻り値を取得するよ：" + product.getPrice());
     }
 
-    @Around("execution(* findProduct())")
+//    @Around("execution(* findProduct())")
     public Product around(ProceedingJoinPoint pjp) throws Throwable {
         System.out.println("Hello Around! before *** メソッドを呼ぶ前に出てくるよ！");
         Signature sig = pjp.getSignature();
@@ -46,7 +46,7 @@ public class MyFirstAspect {
         return p;
     }
 
-    @AfterThrowing(value = "execution(* findProduct())", throwing = "ex")
+//    @AfterThrowing(value = "execution(* findProduct())", throwing = "ex")
     public void afterThrowing(Throwable ex) {
         System.out.println("Hello Throwing! *** 例外になったら出てくるよ！");
     }
