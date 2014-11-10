1475f575275f2c2a05d4050e8ec13636b0adfde6
yukung <yukung.i@gmail.com>
2014-11-10 15:55:27 +0900

    例外処理を追加

--- spring3-introduction/chap06/src/main/java/com/customer/web/controller/CustomerListController.java
+++ spring3-introduction/chap06/src/main/java/com/customer/web/controller/CustomerListController.java
@@ -3,9 +3,12 @@ package com.customer.web.controller;
 import com.customer.biz.domain.Customer;
 import com.customer.biz.service.CustomerService;
 import com.customer.biz.service.DataNotFoundException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,6 +18,8 @@ import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Controller
 public class CustomerListController {
+
+    public static final Logger LOG = LoggerFactory.getLogger(CustomerListController.class);
 
     @Autowired
     private CustomerService customerService;
@@ -36,5 +41,20 @@ public class CustomerListController {
         Customer customer = customerService.findById(customerId);
         model.addAttribute("customer", customer);
         return "customer/detail";
+    }
+
+    // 引数に例外オブジェクトをもらう場合はアノテーションでの指定は不要
+//    @ExceptionHandler(DataNotFoundException.class)
+    @ExceptionHandler
+    public String handleException(DataNotFoundException e) {
+        LOG.warn("Customer is not found", e);
+        return "customer/notfound";
+    }
+
+    // 最も子供の例外クラスが優先で実行される
+    @ExceptionHandler
+    public String handleException(Exception e) {
+        LOG.warn("Exception is threw", e);
+        return "customer/list";
     }
 }
---
+++ spring3-introduction/chap06/src/main/webapp/WEB-INF/views/error.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>例外発生（デバッグ用）</title>
+</head>
+<body>
+<h1>例外発生（デバッグ用）</h1>
+<dl>
+    <dt>例外クラス</dt>
+    <dd>${exception.class.name}</dd>
+    <dt>メッセージ</dt>
+    <dd>${exception.message}</dd>
+</dl>
+
+</body>
+</html>
