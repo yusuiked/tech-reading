735a83d6e18e15cd580208e60094d4351d8d4dba
yukung <yukung.i@gmail.com>
2014-11-27 20:58:56 +0900

    ゼロコンフィグレーションのアノテーションの例を追加。

--- spring3-introduction/chap09/src/main/java/org/yukung/sandbox/sample/actions/HelloAction.java
+++ spring3-introduction/chap09/src/main/java/org/yukung/sandbox/sample/actions/HelloAction.java
@@ -1,5 +1,10 @@
 package org.yukung.sandbox.sample.actions;
 
+import org.apache.struts2.convention.annotation.Action;
+import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+
+@Namespace("/greeting")
 public class HelloAction {
 
 	private String message;
@@ -12,6 +17,7 @@ public class HelloAction {
 		this.message = message;
 	}
 
+	@Action(value="hi", results={@Result(name="success", location="/WEB-INF/jsp/hi.jsp")})
 	public String execute() {
 		message = "World!";
 		return "success";
---
+++ spring3-introduction/chap09/src/main/webapp/WEB-INF/jsp/hi.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<h1>Hi!</h1>
+</body>
+</html>
