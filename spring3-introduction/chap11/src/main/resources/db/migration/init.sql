c8dd88cece5485058b0b1d1a5b7ddc22a4650915
yukung <yukung.i@gmail.com>
2014-11-29 11:46:45 +0900

    JPAとSpringの連携 セットアップ

---
+++ spring3-introduction/chap11/src/main/resources/db/migration/init.sql
@@ -0,0 +1,7 @@
+INSERT INTO t_group (group_id, name) VALUES (1, 'Londo Bell');
+INSERT INTO t_group (group_id, name) VALUES (2, 'Neo Zeon');
+INSERT INTO t_group (group_id, name) VALUES (3, 'Axis');
+
+INSERT INTO t_member (name, detail, group_id) VALUES ('Amuro Ray', 'Pilot of Nu Gundom', 1);
+INSERT INTO t_member (name, detail, group_id) VALUES ('Char Aznable', 'Pilot of Sazabi', 2);
+INSERT INTO t_member (name, detail, group_id) VALUES ('Haman Karn', 'Pilot of Qubeley', 3);
