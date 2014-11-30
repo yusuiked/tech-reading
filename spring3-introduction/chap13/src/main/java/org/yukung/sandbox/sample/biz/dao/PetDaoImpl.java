6b36f9c17b237c08de4eee1a7816018fa0d0a511
yukung <yukung.i@gmail.com>
2014-12-01 01:23:46 +0900

    MyBatis3とSpringの連携

--- spring3-introduction/chap13/src/main/java/org/yukung/sandbox/sample/biz/dao/PetDaoImpl.java
+++ spring3-introduction/chap13/src/main/java/org/yukung/sandbox/sample/biz/dao/PetDaoImpl.java
@@ -1,16 +1,18 @@
 package org.yukung.sandbox.sample.biz.dao;
 
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.yukung.sandbox.sample.biz.domain.Pet;
 
+@Repository
 public class PetDaoImpl implements PetDao {
+
+	@Autowired
+	private SqlSession session;
 
 	@Override
 	public Pet findPetById(Integer id) {
-		return null;
-	}
-
-	public Pet findPetById(SqlSession session, Integer id) {
 		return session.selectOne("org.yukung.sandbox.sample.biz.dao.PetMapper.selectPet", id);
 	}
 }
