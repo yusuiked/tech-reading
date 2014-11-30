141a7012e7edaac2985c7ceac98a9c6a67431ebb
yukung <yukung.i@gmail.com>
2014-11-30 12:26:22 +0900

    JPAとSpring連携版のDAOアクセス

--- spring3-introduction/chap11/src/main/java/org/yukung/sandbox/sample/biz/dao/GroupDaoImpl.java
+++ spring3-introduction/chap11/src/main/java/org/yukung/sandbox/sample/biz/dao/GroupDaoImpl.java
@@ -3,47 +3,34 @@ package org.yukung.sandbox.sample.biz.dao;
 import java.util.List;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.stereotype.Repository;
 import org.yukung.sandbox.sample.biz.domain.Group;
 
+@Repository
 public class GroupDaoImpl implements GroupDao {
+
+	@PersistenceContext
+	private EntityManager em;
 
 	@Override
 	public List<Group> findGroupByName(String name) {
-		return null;
+		Query query = em.createQuery("select g from Group g where g.name like ?1");
+		query.setParameter(1, "%"+name+"%");
+		return query.getResultList();
 	}
 
 	@Override
 	public void insertGroup(Group group) {
-
-	}
-
-	@Override
-	public void removeGroup(Group group) {
-
-	}
-
-	public List<Group> findGroupByName(EntityManager em, String name) {
-		Query query = em.createQuery("select g from Group g where g.name like ?1");
-		query.setParameter(1,  "%"+name+"%");
-		return query.getResultList();
-	}
-
-	public void insertGroup(EntityManager em, Group group) {
 		em.persist(group);
 		em.flush();
 	}
 
-	public void removeGroup(EntityManager em, Group group) {
+	@Override
+	public void removeGroup(Group group) {
 		em.remove(group);
 		em.flush();
-	}
-
-	private EntityManager createEntityManager() {
-		EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("manager1");
-		return emfactory.createEntityManager();
 	}
 }
--- spring3-introduction/chap11/src/test/java/org/yukung/sandbox/sample/biz/dao/GroupDaoImplTest.java
+++ spring3-introduction/chap11/src/test/java/org/yukung/sandbox/sample/biz/dao/GroupDaoImplTest.java
@@ -5,36 +5,35 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 import org.yukung.sandbox.sample.biz.domain.Group;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:META-INF/spring/applicationContext.xml")
+@Transactional
 public class GroupDaoImplTest {
 
-	private GroupDaoImpl groupDao;
-	private EntityManager em;
+	@Autowired
+	private GroupDao groupDao;
 
 	@Before
 	public void setUp() throws Exception {
-		groupDao = new GroupDaoImpl();
-		EntityManagerFactory emFactory = Persistence.createEntityManagerFactory("manager1");
-		em = emFactory.createEntityManager();
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		em.close();
 	}
 
 	@Test
 	public void testFindGroupByName() {
-		List<Group> groups = groupDao.findGroupByName(em, "Zeon");
+		List<Group> groups = groupDao.findGroupByName("Zeon");
 		assertThat(groups, is(notNullValue()));
 		assertThat(groups.size(), is(1));
 		for (Group group : groups) {
@@ -45,38 +44,24 @@ public class GroupDaoImplTest {
 
 	@Test
 	public void testInsertGroup() throws Exception {
-		List<Group> beforeAddition = groupDao.findGroupByName(em, "Titans");
+		List<Group> beforeAddition = groupDao.findGroupByName("Titans");
 		assertThat(beforeAddition, is(notNullValue()));
 		assertThat(beforeAddition.size(), is(0));
 		Group group = new Group();
 		group.setName("Titans");
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
-		try {
-			groupDao.insertGroup(em, group);
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		}
-		List<Group> afterAddition = groupDao.findGroupByName(em, "Titans");
+		groupDao.insertGroup(group);
+		List<Group> afterAddition = groupDao.findGroupByName("Titans");
 		assertThat(afterAddition, is(notNullValue()));
 		assertThat(afterAddition.size(), is(1));
 	}
 
 	@Test
 	public void testRemoveGroup() throws Exception {
-		List<Group> beforeAddition = groupDao.findGroupByName(em, "Zeon");
+		List<Group> beforeAddition = groupDao.findGroupByName("Zeon");
 		assertThat(beforeAddition, is(notNullValue()));
 		assertThat(beforeAddition.size(), is(1));
-		EntityTransaction tx = em.getTransaction();
-		tx.begin();
-		try {
-			groupDao.removeGroup(em, beforeAddition.get(0));
-			tx.commit();
-		} catch (Exception e) {
-			tx.rollback();
-		}
-		List<Group> afterAddition = groupDao.findGroupByName(em, "Titans");
+		groupDao.removeGroup(beforeAddition.get(0));
+		List<Group> afterAddition = groupDao.findGroupByName("Titans");
 		assertThat(afterAddition, is(notNullValue()));
 		assertThat(afterAddition.size(), is(0));
 	}
