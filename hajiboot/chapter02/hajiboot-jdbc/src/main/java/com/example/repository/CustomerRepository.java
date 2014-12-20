d286e728ebe5fea55b848956201e04064025039b
yukung <yukung.i@gmail.com>
2014-12-20 21:35:30 +0900

    CustomerRepository の save に自動生成IDの設定ロジックを追加

--- hajiboot/chapter02/hajiboot-jdbc/src/main/java/com/example/repository/CustomerRepository.java
+++ hajiboot/chapter02/hajiboot-jdbc/src/main/java/com/example/repository/CustomerRepository.java
@@ -2,12 +2,16 @@ package com.example.repository;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,6 +23,16 @@ public class CustomerRepository {
 	@Autowired
 	NamedParameterJdbcTemplate jdbcTemplate;
 
+	SimpleJdbcInsert insert;
+
+	@PostConstruct
+	public void init() {
+		insert =
+				new SimpleJdbcInsert((JdbcTemplate) jdbcTemplate.getJdbcOperations())
+					.withTableName("customers")
+					.usingGeneratedKeyColumns("id");
+	}
+
 	private static final RowMapper<Customer> customerRowMapper = (rs, i) -> {
 		Integer id = rs.getInt("id");
 		String firstName = rs.getString("first_name");
@@ -28,36 +42,33 @@ public class CustomerRepository {
 
 	public List<Customer> findAll() {
 		return jdbcTemplate.query(
-				"SELECT id, first_name, last_name FROM customers ORDER BY id",
-				customerRowMapper);
+			"SELECT id, first_name, last_name FROM customers ORDER BY id",
+			customerRowMapper);
 	}
 
 	public Customer findOne(Integer id) {
-		SqlParameterSource param = new MapSqlParameterSource().addValue("id",
-				id);
-		return jdbcTemplate
-				.queryForObject(
-						"SELECT id, first_name, last_name FROM customers WHERE id = :id",
-						param, customerRowMapper);
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
+		return jdbcTemplate.queryForObject(
+			"SELECT id, first_name, last_name FROM customers WHERE id = :id",
+			param,
+			customerRowMapper);
 	}
 
 	public Customer save(Customer customer) {
 		SqlParameterSource param = new BeanPropertySqlParameterSource(customer);
 		if (customer.getId() == null) {
-			jdbcTemplate
-					.update("INSERT INTO customers(first_name, last_name) VALUES(:firstName, :lastName)",
-							param);
+			Number key = insert.executeAndReturnKey(param);
+			customer.setId(key.intValue());
 		} else {
-			jdbcTemplate
-					.update("UPDATE customers SET first_name=:firstName, last_name=:lastName WHERE id=:id",
-							param);
+			jdbcTemplate.update(
+				"UPDATE customers SET first_name=:firstName, last_name=:lastName WHERE id=:id",
+				param);
 		}
 		return customer;
 	}
 
 	public void delete(Integer id) {
-		SqlParameterSource param = new MapSqlParameterSource().addValue("id",
-				id);
+		SqlParameterSource param = new MapSqlParameterSource().addValue("id", id);
 		jdbcTemplate.update("DELETE FROM customers WHERE id=:id", param);
 	}
 }
