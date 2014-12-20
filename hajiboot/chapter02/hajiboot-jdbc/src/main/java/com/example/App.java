6d80c03d85a3cc100389c508e987a7eebab8a552
yukung <yukung.i@gmail.com>
2014-12-20 19:16:49 +0900

    JdbcTemplate を使ったオブジェクト・マッピング

--- hajiboot/chapter02/hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot/chapter02/hajiboot-jdbc/src/main/java/com/example/App.java
@@ -1,11 +1,17 @@
 package com.example;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+import com.example.domain.Customer;
 
 @EnableAutoConfiguration
 public class App implements CommandLineRunner {
@@ -14,10 +20,16 @@ public class App implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		String sql = "SELECT :a + :b";
-		MapSqlParameterSource param =
-				new MapSqlParameterSource().addValue("a", 100).addValue("b", 200);
-		Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
+		MapSqlParameterSource param = new MapSqlParameterSource().addValue("id", 1);
+		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
+
+			@Override
+			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
+				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs
+					.getString("last_name"));
+			}
+		});
 
 		System.out.println("result = " + result);
 	}
---
+++ hajiboot/chapter02/hajiboot-jdbc/src/main/java/com/example/domain/Customer.java
@@ -0,0 +1,14 @@
+package com.example.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Customer {
+	private Integer id;
+
+	private String firstName;
+
+	private String lastName;
+}
