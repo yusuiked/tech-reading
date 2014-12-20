a3f0ef856a39dd87f0b36ddbd462cca9244fd5ae
yukung <yukung.i@gmail.com>
2014-12-20 17:52:08 +0900

    CommandLineRunner の利用

--- hajiboot/chapter02/hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot/chapter02/hajiboot-di/src/main/java/com/example/App.java
@@ -1,19 +1,32 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
 
 @EnableAutoConfiguration
 @ComponentScan
-public class App {
+public class App implements CommandLineRunner {
+	@Autowired
+	ArgumentResolver argumentResolver;
+	@Autowired
+	Calculator calculator;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.print("Enter 2 numbers like 'a b' : ");
+		Argument argument = argumentResolver.resolve(System.in);
+		int result = calculator.calc(argument.getA(), argument.getB());
+		System.out.println("result = " + result);
+	}
+
 	public static void main(String[] args) {
-		try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-			Frontend frontend = context.getBean(Frontend.class);
-			frontend.run();
-		}
+		SpringApplication.run(App.class, args);
 	}
 }
