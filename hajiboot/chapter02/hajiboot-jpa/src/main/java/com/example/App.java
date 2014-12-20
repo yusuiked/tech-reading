ff475e362e8383ce8aab649686fbd35b0845437f
yukung <yukung.i@gmail.com>
2014-12-20 23:37:09 +0900

    2.3.4 ページング処理の実装

--- hajiboot/chapter02/hajiboot-jpa/src/main/java/com/example/App.java
+++ hajiboot/chapter02/hajiboot-jpa/src/main/java/com/example/App.java
@@ -5,6 +5,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import com.example.domain.Customer;
 import com.example.repository.CustomerRepository;
@@ -20,9 +23,14 @@ public class App implements CommandLineRunner {
 		// データ追加
 		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
 		System.out.println(created + " is created!");
-		// データ表示
-//		customerRepository.findAll().forEach(System.out::println);
-		customerRepository.findAllOrderByName().forEach(System.out::println);
+		// ページング処理
+		Pageable pageable = new PageRequest(0, 3);
+		Page<Customer> page = customerRepository.findAll(pageable);
+		System.out.println("1ページあたりのデータ数 = " + page.getSize());
+		System.out.println("現在のページ = " + page.getNumber());
+		System.out.println("全ページ数 = " + page.getTotalPages());
+		System.out.println("全データ数 = " + page.getTotalElements());
+		page.getContent().forEach(System.out::println);
 	}
 
 	public static void main(String[] args) {
