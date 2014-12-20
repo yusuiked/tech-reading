9a60c3703a025c254059e2af55268126f8d95e1b
yukung <yukung.i@gmail.com>
2014-12-20 19:19:13 +0900

    JdbcTemplate を使ったオブジェクト・マッピング（ラムダ式版）

--- hajiboot/chapter02/hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot/chapter02/hajiboot-jdbc/src/main/java/com/example/App.java
@@ -1,13 +1,9 @@
 package com.example;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
@@ -22,14 +18,12 @@ public class App implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		String sql = "SELECT id, first_name, last_name FROM customers WHERE id = :id";
 		MapSqlParameterSource param = new MapSqlParameterSource().addValue("id", 1);
-		Customer result = jdbcTemplate.queryForObject(sql, param, new RowMapper<Customer>() {
-
-			@Override
-			public Customer mapRow(ResultSet rs, int rowNum) throws SQLException {
-				return new Customer(rs.getInt("id"), rs.getString("first_name"), rs
-					.getString("last_name"));
-			}
-		});
+		Customer result =
+				jdbcTemplate.queryForObject(
+					sql,
+					param,
+					(rs, rowNum) -> new Customer(rs.getInt("id"), rs
+						.getString("first_name"), rs.getString("last_name")));
 
 		System.out.println("result = " + result);
 	}
