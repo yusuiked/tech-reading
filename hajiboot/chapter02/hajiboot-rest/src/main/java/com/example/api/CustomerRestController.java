57deda08e7bcd13f36cdfe5ca43f78dd19e24ca7
yukung <yukung.i@gmail.com>
2014-12-23 11:05:31 +0900

    3.2.2 顧客新規作成、顧客１件更新、顧客１件削除 API の実装

--- hajiboot/chapter02/hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot/chapter02/hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -3,9 +3,12 @@ package com.example.api;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.domain.Customer;
@@ -28,4 +31,23 @@ public class CustomerRestController {
 		Customer customer = customerService.findOne(id);
 		return customer;
 	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	Customer postCustomers(@RequestBody Customer customer) {
+		return customerService.create(customer);
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+	Customer putCustomer(@PathVariable Integer id, @RequestBody Customer customer) {
+		customer.setId(id);
+		return customerService.update(customer);
+	}
+
+	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	void deleteCustomer(@PathVariable Integer id) {
+		customerService.delete(id);
+	}
+
 }
