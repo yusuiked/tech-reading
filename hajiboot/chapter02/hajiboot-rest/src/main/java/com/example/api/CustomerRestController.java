14a0719a8f83136a862645a9c210ef9286463d65
yukung <yukung.i@gmail.com>
2014-12-23 11:52:00 +0900

    Location ヘッダにリソースの URI を設定

--- hajiboot/chapter02/hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
+++ hajiboot/chapter02/hajiboot-rest/src/main/java/com/example/api/CustomerRestController.java
@@ -1,15 +1,19 @@
 package com.example.api;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.example.domain.Customer;
 import com.example.service.CustomerService;
@@ -33,9 +37,12 @@ public class CustomerRestController {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.CREATED)
-	Customer postCustomers(@RequestBody Customer customer) {
-		return customerService.create(customer);
+	ResponseEntity<Customer> postCustomers(@RequestBody Customer customer, UriComponentsBuilder uriBuilder) {
+		Customer created = customerService.create(customer);
+		URI location = uriBuilder.path("api/customers/{id}").buildAndExpand(created.getId()).toUri();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(location);
+		return new ResponseEntity<>(created, headers, HttpStatus.CREATED);
 	}
 
 	@RequestMapping(value = "{id}", method = RequestMethod.PUT)
