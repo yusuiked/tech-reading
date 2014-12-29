8d0cb84c7774cfbdd2cb5ff46095db2ff51db4c6
yukung <yukung.i@gmail.com>
2014-12-29 16:51:18 +0900

    1-10のうち偶数を出力する（メソッド参照）

--- java-engineer-training-readings/feature3/chapter03/src/main/java/org/yukung/sandbox/stream/StreamPractice.java
+++ java-engineer-training-readings/feature3/chapter03/src/main/java/org/yukung/sandbox/stream/StreamPractice.java
@@ -14,6 +14,6 @@ public class StreamPractice {
     private void printEven() {
         IntStream.rangeClosed(1, 10)
                 .filter(n -> n % 2 == 0)
-                .forEach(n -> System.out.println(n));
+                .forEach(System.out::println);
     }
 }
