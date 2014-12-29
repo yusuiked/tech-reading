7369cfb79372f472f3c22a54066abd00a88adf70
yukung <yukung.i@gmail.com>
2014-12-29 18:00:37 +0900

    部署別の社員のリスト（マップ）に変換する

--- java-engineer-training-readings/feature3/chapter03/src/main/java/org/yukung/sandbox/stream/StreamPractice.java
+++ java-engineer-training-readings/feature3/chapter03/src/main/java/org/yukung/sandbox/stream/StreamPractice.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.OptionalInt;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -26,12 +27,18 @@ public class StreamPractice {
         Path path = Paths.get("./");
         System.out.println(containsTextFile(path));
         printSeparator();
-        List<Emp> emps = Arrays.asList(new Emp(500), new Emp(1000), new Emp(800));
+        List<Emp> emps = Arrays.asList(new Emp(500, new Dep("営業部")), new Emp(1000, new Dep("総務部")), new Emp(800, new Dep("営業部")));
         empsOfHighIncome(emps).forEach(System.out::println);
         printSeparator();
         Student[] students = new Student[]{new Student("山田太郎"), new Student("鈴木花子"), new Student("佐藤一郎")};
         System.out.println(toStringByName(students));
         printSeparator();
+        empsByDep(emps).forEach((k, v) -> System.out.printf("%s:%s%n", k, v));
+    }
+
+    private Map<Dep, List<Emp>> empsByDep(List<Emp> emps) {
+        return emps.stream()
+                .collect(Collectors.groupingBy(emp -> emp.getDep()));   // キーを取得するための Function インターフェースのインスタンス
     }
 
     private String toStringByName(Student[] students) {
@@ -85,6 +92,12 @@ public class StreamPractice {
 
     private static class Emp {
         private int income;
+        private Dep dep;
+
+        public Emp(int income, Dep dep) {
+            this.income = income;
+            this.dep = dep;
+        }
 
         public int getIncome() {
             return income;
@@ -94,15 +107,42 @@ public class StreamPractice {
             this.income = income;
         }
 
-        public Emp(int income) {
+        public Dep getDep() {
+            return dep;
+        }
 
-            this.income = income;
+        public void setDep(Dep dep) {
+            this.dep = dep;
         }
 
         @Override
         public String toString() {
             return "Emp{" +
                     "income=" + income +
+                    '}';
+        }
+    }
+
+    private static class Dep {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public Dep(String name) {
+
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Dep{" +
+                    "name='" + name + '\'' +
                     '}';
         }
     }
