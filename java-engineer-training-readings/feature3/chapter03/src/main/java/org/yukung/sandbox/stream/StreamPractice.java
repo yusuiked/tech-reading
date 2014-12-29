47c60babfdc57d69832680299a8e7c81a13d97b6
yukung <yukung.i@gmail.com>
2014-12-29 16:49:51 +0900

    実例 Stream API

---
+++ java-engineer-training-readings/feature3/chapter03/src/main/java/org/yukung/sandbox/stream/StreamPractice.java
@@ -0,0 +1,19 @@
+package org.yukung.sandbox.stream;
+
+import java.util.stream.IntStream;
+
+public class StreamPractice {
+    public static void main(String[] args) {
+        new StreamPractice().run();
+    }
+
+    private void run() {
+        printEven();
+    }
+
+    private void printEven() {
+        IntStream.rangeClosed(1, 10)
+                .filter(n -> n % 2 == 0)
+                .forEach(n -> System.out.println(n));
+    }
+}
