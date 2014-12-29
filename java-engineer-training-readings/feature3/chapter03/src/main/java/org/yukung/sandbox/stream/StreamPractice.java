d75a4a93aadceaddfdf3f5057cec5141dfa54cb3
yukung <yukung.i@gmail.com>
2014-12-29 16:59:22 +0900

    学生リストから名前を出力する

--- java-engineer-training-readings/feature3/chapter03/src/main/java/org/yukung/sandbox/stream/StreamPractice.java
+++ java-engineer-training-readings/feature3/chapter03/src/main/java/org/yukung/sandbox/stream/StreamPractice.java
@@ -1,5 +1,7 @@
 package org.yukung.sandbox.stream;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class StreamPractice {
@@ -9,6 +11,35 @@ public class StreamPractice {
 
     private void run() {
         printEven();
+        printSeparator();
+        printStudentsName();
+        printSeparator();
+    }
+
+    private void printStudentsName() {
+        class Student {
+            private String name;
+
+            public Student(String name) {
+                this.name = name;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+        List<Student> students = Arrays.asList(new Student("Ichiro"), new Student("Jiro"), new Student("Saburo"));
+        students.stream()
+                .map(s -> s.getName())
+                .forEach(n -> System.out.println(n));
+    }
+
+    private void printSeparator() {
+        System.out.println("-------------------------------");
     }
 
     private void printEven() {
