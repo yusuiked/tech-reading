9fc8e2243e1f16fb8121a0a6fa9f78f77cadfc19
yukung <yukung.i@gmail.com>
2015-02-08 17:48:48 +0900

    リスト3.58 wordCount.groovyの改良版

---
+++ programming-groovy/chapter03/customizeWordCount.groovy
@@ -0,0 +1,10 @@
+// 指定したテキストファイル中の単語の出現回数を一覧表示する（改良版）
+def map = [:].withDefault{0}
+new File(args[0]).eachLine {
+    it.split(/\s+/).each {
+        map[it]++
+    }
+}
+map.entrySet().sort{it.value}.each {
+    println "${it.value}: [${it.key}]"
+}
