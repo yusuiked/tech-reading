0feee0718d70f1dafc4f5260daaeae34da31c533
yukung <yukung.i@gmail.com>
2015-02-18 00:33:16 +0900

    1.3 JUnit テストを始めよう

---
+++ junit-practice-introduction/chapter01/junit-tutorial/src/junit/tutorial/Calculator.java
@@ -0,0 +1,11 @@
+package junit.tutorial;
+
+public class Calculator {
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public int devide(int x, int y) {
+		return x / y;
+	}
+}
---
+++ junit-practice-introduction/chapter01/junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -0,0 +1,14 @@
+package junit.tutorial;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	
+}
