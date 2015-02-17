c217c15f46fa6de4684efde3bd3a347679d4a311
yukung <yukung.i@gmail.com>
2015-02-18 00:41:38 +0900

    1.4 テストコードの記述
    乗算メソッドのテストを追加

--- junit-practice-introduction/chapter01/junit-tutorial/test/junit/tutorial/CalculatorTest.java
+++ junit-practice-introduction/chapter01/junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -7,10 +7,18 @@ import org.junit.Test;
 
 public class CalculatorTest {
 	@Test
-	public void multiplyで乗算結果が取得できる() {
+	public void multiplyで3と4の乗算結果が取得できる() {
 		Calculator calc = new Calculator();
 		int expected = 12;
 		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
+	}
+	
+	@Test
+	public void multiplyで5と7の乗算結果が取得できる() throws Exception {
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(5, 7);
 		assertThat(actual, is(expected));
 	}
 }
