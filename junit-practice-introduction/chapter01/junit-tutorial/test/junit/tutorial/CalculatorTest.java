d6e7b22296ce3bd1f9c9bd6e0550f990d01adb13
yukung <yukung.i@gmail.com>
2015-02-18 00:38:02 +0900

    1.4 テストコードの記述
    テストコードの追加

--- junit-practice-introduction/chapter01/junit-tutorial/test/junit/tutorial/CalculatorTest.java
+++ junit-practice-introduction/chapter01/junit-tutorial/test/junit/tutorial/CalculatorTest.java
@@ -1,14 +1,16 @@
 package junit.tutorial;
 
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 public class CalculatorTest {
-	
 	@Test
 	public void multiplyで乗算結果が取得できる() {
-		fail("Not yet implemented");
+		Calculator calc = new Calculator();
+		int expected = 12;
+		int actual = calc.multiply(3, 4);
+		assertThat(actual, is(expected));
 	}
-	
 }
