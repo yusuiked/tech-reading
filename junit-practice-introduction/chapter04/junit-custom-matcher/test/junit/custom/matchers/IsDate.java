0a28beede4d7f7833f5b22108e584fc3c6304532
yukung <yukung.i@gmail.com>
2015-02-25 19:51:25 +0900

    カスタムMatcherのコンストラクタを定義する

--- junit-practice-introduction/chapter04/junit-custom-matcher/test/junit/custom/matchers/IsDate.java
+++ junit-practice-introduction/chapter04/junit-custom-matcher/test/junit/custom/matchers/IsDate.java
@@ -8,6 +8,16 @@ import org.hamcrest.Matcher;
 
 public class IsDate extends BaseMatcher<Date> {
 
+	private final int yyyy;
+	private final int mm;
+	private final int dd;
+
+	public IsDate(int yyyy, int mm, int dd) {
+		this.yyyy = yyyy;
+		this.mm = mm;
+		this.dd = dd;
+	}
+
 	public boolean matches(Object actual) {
 		return false;
 	}
@@ -17,6 +27,6 @@ public class IsDate extends BaseMatcher<Date> {
 	}
 
 	public static Matcher<Date> dateOf(int yyyy, int mm, int dd) {
-		return new IsDate();
+		return new IsDate(yyyy, mm, dd);
 	}
 }
