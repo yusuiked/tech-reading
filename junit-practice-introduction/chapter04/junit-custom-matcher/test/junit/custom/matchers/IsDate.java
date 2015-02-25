8b76705294d565159e527256b0a1319a8340ad66
yukung <yukung.i@gmail.com>
2015-02-25 19:46:36 +0900

    カスタムMatcherのファクトリメソッドを作成する

--- junit-practice-introduction/chapter04/junit-custom-matcher/test/junit/custom/matchers/IsDate.java
+++ junit-practice-introduction/chapter04/junit-custom-matcher/test/junit/custom/matchers/IsDate.java
@@ -1,18 +1,22 @@
 package junit.custom.matchers;
 
+import java.util.Date;
+
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 
-public class IsDate extends BaseMatcher {
+public class IsDate extends BaseMatcher<Date> {
 
-	public boolean matches(Object arg0) {
-		// TODO Auto-generated method stub
+	public boolean matches(Object actual) {
 		return false;
 	}
 
-	public void describeTo(Description arg0) {
-		// TODO Auto-generated method stub
+	public void describeTo(Description desc) {
 
 	}
 
+	public static Matcher<Date> dateOf(int yyyy, int mm, int dd) {
+		return new IsDate();
+	}
 }
