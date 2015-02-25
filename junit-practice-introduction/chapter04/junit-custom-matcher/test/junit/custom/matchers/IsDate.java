b96b61955a1a2efa8628be7dea0689dddbab5ea0
yukung <yukung.i@gmail.com>
2015-02-25 16:55:23 +0900

    カスタムMatcherの作成手順
    isDateクラスを作成する

---
+++ junit-practice-introduction/chapter04/junit-custom-matcher/test/junit/custom/matchers/IsDate.java
@@ -0,0 +1,18 @@
+package junit.custom.matchers;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+
+public class IsDate extends BaseMatcher {
+
+	public boolean matches(Object arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void describeTo(Description arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
