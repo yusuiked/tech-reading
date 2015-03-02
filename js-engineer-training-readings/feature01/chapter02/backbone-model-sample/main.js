e9f393aa6acff808496b839f5b99e54aac696f4d
yukung <yukung.i@gmail.com>
2015-03-02 13:35:21 +0900

    第2章 モデル実装入門
    Backbone.Modelオブジェクトの定義、初期化

---
+++ js-engineer-training-readings/feature01/chapter02/backbone-model-sample/main.js
@@ -0,0 +1,22 @@
+var Contact = Backbone.Model.extend({
+    defaults: {
+        firstName: '',
+        lastName: '',
+        email: ''
+    },
+    initialize: function() {
+        console.log('Contactが初期化されました。');
+    }
+});
+
+var contact = new Contact({
+    firstName: 'Alice',
+    lastName: 'Henderson',
+    email: 'alice@example.com'
+});
+
+console.log(JSON.stringify(contact, null, 2));
+
+var contact2 = new Contact();
+
+console.log(JSON.stringify(contact2, null, 2));
