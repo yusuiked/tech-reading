65b2d1e2b69fdc3782af7bf8f437e01edc77a514
yukung <yukung.i@gmail.com>
2015-03-06 21:51:11 +0900

    attributes への直接アクセス

--- js-engineer-training-readings/feature01/chapter02/backbone-model-sample/main.js
+++ js-engineer-training-readings/feature01/chapter02/backbone-model-sample/main.js
@@ -1,3 +1,4 @@
+// Backbone.Model オブジェクトの定義
 var Contact = Backbone.Model.extend({
     defaults: {
         firstName: '',
@@ -8,19 +9,19 @@ var Contact = Backbone.Model.extend({
         console.log('Contactが初期化されました。');
     }
 });
-
+// Model オブジェクトの初期化
 var contact = new Contact({
     firstName: 'Alice',
     lastName: 'Henderson',
     email: 'alice@example.com'
 });
-
+// 出力
 console.log(JSON.stringify(contact, null, 2));
 
 var contact2 = new Contact();
-
+// 初期値で出力
 console.log(JSON.stringify(contact2, null, 2));
-
+// null で生成
 var contact3 = new Contact();
 contact3.set('firstName', 'Alice');
 contact3.set({
@@ -31,5 +32,13 @@ contact3.set({
 console.log(JSON.stringify(contact3, null, 2));
 
 console.log(contact3.get('firstName'));
+// プロパティが存在するかを確認
 console.log(contact3.has('lastName'));
 console.log(contact3.has('email'));
+
+// attributes への直接アクセス
+contact3.attributes.email = 'yukung.i@gmail.com';
+console.log(contact3.get('email'));
+// set()された値を attributes から直接取得
+contact3.set('lastName', 'Sanders');
+console.log(contact3.attributes.lastName);
