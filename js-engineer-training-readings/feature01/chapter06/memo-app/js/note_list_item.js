8ce5556d349b761b4fc009e87515be88dcc041c5
yukung <yukung.i@gmail.com>
2015-03-14 19:23:49 +0900

    メモの削除

--- js-engineer-training-readings/feature01/chapter06/memo-app/js/note_list_item.js
+++ js-engineer-training-readings/feature01/chapter06/memo-app/js/note_list_item.js
@@ -1,10 +1,24 @@
 App.NoteListItemView = Backbone.View.extend({
 	tagName: 'tr',
+	initialize: function() {
+		// モデルの destroy イベントを監視して
+		// Backbone.View の remove() メソッドを呼び出す
+		this.listenTo(this.model, 'destroy', this.remove);
+	},
+	// [Delete]ボタンを監視して
+	// onClickDelete() メソッドを呼び出す
+	events: {
+		'click .js-delete': 'onClickDelete'
+	},
 	render: function() {
 		var template = $('#noteListItemView-template').html();
 		var compiled = _.template(template);
 		var html = compiled(this.model.toJSON());
 		this.$el.html(html);
 		return this;
+	},
+	onClickDelete: function() {
+		// モデルを削除する
+		this.model.destroy();
 	}
 });
