a01401e8b181596c16932c909de1b17643b07138
yukung <yukung.i@gmail.com>
2015-07-05 22:59:28 +0900

    日経ソフトウェア2015年8月号のVisual Studio特集 デジタル時計を作成する

---
+++ nikkei-software/201508/MyClock/MyClock/ClockViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClock
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        Clock clock = new Clock();
+
+        string digitalDisplay = string.Empty;
+
+        public string DigitalDisplay
+        {
+            get
+            {
+                return digitalDisplay;
+            }
+            private set
+            {
+                if (digitalDisplay != value)
+                {
+                    digitalDisplay = value;
+                    RaisePropertyChanged("DigitalDisplay");
+                }
+            }
+        }
+
+        public ClockViewModel()
+        {
+            clock.Tick += (sender, currentTime) => DigitalDisplay = currentTime.ToLongTimeString();
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
