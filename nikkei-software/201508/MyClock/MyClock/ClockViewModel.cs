bbd7071497242486fc1f64b68531d90faaa2f6ec
yukung <yukung.i@gmail.com>
2015-07-05 23:22:09 +0900

    日経ソフトウェア2015年8月号のVisual Studio特集 時計ビューで針を動かす

--- nikkei-software/201508/MyClock/MyClock/ClockViewModel.cs
+++ nikkei-software/201508/MyClock/MyClock/ClockViewModel.cs
@@ -31,9 +31,74 @@ namespace MyClock
             }
         }
 
+        double hourHandAngle = 0.0;
+
+        public double HourHandAngle
+        {
+            get
+            {
+                return hourHandAngle;
+            }
+            private set
+            {
+                if (hourHandAngle != value)
+                {
+                    hourHandAngle = value;
+                    RaisePropertyChanged("HourHandAngle");
+                }
+            }
+        }
+
+        double minuteHandAngle = 0.0;
+
+        public double MinuteHandAngle
+        {
+            get
+            {
+                return minuteHandAngle;
+            }
+            private set
+            {
+                if (minuteHandAngle != value)
+                {
+                    minuteHandAngle = value;
+                    RaisePropertyChanged("MinuteHandAngle");
+                }
+            }
+        }
+
+        double secondHandAngle = 0.0;
+
+        public double SecondHandAngle
+        {
+            get
+            {
+                return secondHandAngle;
+            }
+            private set
+            {
+                if (secondHandAngle != value)
+                {
+                    secondHandAngle = value;
+                    RaisePropertyChanged("SecondHandAngle");
+                }
+            }
+        }
+
+        void UpdateHandAngle(DateTime time)
+        {
+            SecondHandAngle = time.Second * 360.0 / 60.0;
+            MinuteHandAngle = (time.Minute + time.Second / 60.0) * 360.0 / 60.0;
+            HourHandAngle = (time.Hour + time.Minute / 60.0) * 360.0 / 12.0;
+        }
+
         public ClockViewModel()
         {
-            clock.Tick += (sender, currentTime) => DigitalDisplay = currentTime.ToLongTimeString();
+            clock.Tick += (sender, currentTime) =>
+            {
+                DigitalDisplay = currentTime.ToLongTimeString();
+                UpdateHandAngle(currentTime);
+            };
         }
 
         private void RaisePropertyChanged(string propertyName)
