7f07a8aa553b191dfd83912f8fab4afbf9a2a113
yukung <yukung.i@gmail.com>
2015-07-05 19:38:12 +0900

    日経ソフトウェア2015年8月号のVisual Studio特集 デジタル時計を作成する

---
+++ nikkei-software/201508/StopWatch/StopWatch/MainForm.Designer.vb
@@ -0,0 +1,59 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.displayTimeTextBox = New System.Windows.Forms.TextBox()
+        Me.timer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'displayTimeTextBox
+        '
+        Me.displayTimeTextBox.BackColor = System.Drawing.Color.White
+        Me.displayTimeTextBox.Font = New System.Drawing.Font("MS UI Gothic", 27.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(128, Byte))
+        Me.displayTimeTextBox.Location = New System.Drawing.Point(12, 12)
+        Me.displayTimeTextBox.Name = "displayTimeTextBox"
+        Me.displayTimeTextBox.ReadOnly = True
+        Me.displayTimeTextBox.Size = New System.Drawing.Size(260, 44)
+        Me.displayTimeTextBox.TabIndex = 0
+        '
+        'timer
+        '
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.displayTimeTextBox)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "MainForm"
+        Me.Text = "MyStopWatch"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents displayTimeTextBox As TextBox
+    Friend WithEvents timer As Timer
+End Class
---
+++ nikkei-software/201508/StopWatch/StopWatch/MainForm.vb
@@ -0,0 +1,9 @@
+﻿Public Class MainForm
+    Private Sub MainForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        timer.Start()
+    End Sub
+
+    Private Sub timer_Tick(sender As Object, e As EventArgs) Handles timer.Tick
+        displayTimeTextBox.Text = DateTime.Now.ToLongTimeString()
+    End Sub
+End Class
