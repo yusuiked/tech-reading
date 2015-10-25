b86ad003cee7ea5f351f80dbdca3ec9e3d7fbc8d
yukung <yukung.i@gmail.com>
2015-10-25 20:27:42 +0900

    リスト5.1 実行可能なテストクラス

---
+++ junit-practice-introduction/chapter08/junit-test-runner/src/test/java/CalcTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+import org.junit.runner.JUnitCore;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * 実行可能なテストクラス
+ */
+public class CalcTest {
+    @Test
+    public void addに3と4を与えると7を返す() throws Exception {
+        Calc sut = new Calc();
+        assertThat(sut.add(3, 4), is(7));
+    }
+
+    public static void main(String[] args) {
+        JUnitCore.main(CalcTest.class.getName());
+    }
+
+    private static class Calc {
+        public int add(int x, int y) {
+            return x + y;
+        }
+    }
+}
