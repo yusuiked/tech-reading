6b27d691e9d46ab564a0bd98b9ab5d5f485a752c
yukung <yukung.i@gmail.com>
2015-11-07 16:42:15 +0900

    リスト8.14 DataPoints アノテーションの利用

---
+++ junit-practice-introduction/chapter08/junit-test-parameterized/src/test/java/CalculatorDataPointsTest.java
@@ -0,0 +1,35 @@
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(Theories.class)
+public class CalculatorDataPointsTest {
+    @DataPoints
+    public static Fixture[] PARAMS = {
+            new Fixture(3, 4, 7),
+            new Fixture(0, 5, 5),
+            new Fixture(-3, 1, -2)
+    };
+
+    @Theory
+    public void add(Fixture p) throws Exception {
+        Calculator sut = new Calculator();
+        assertThat(sut.add(p.x, p.y), is(p.expected));
+    }
+
+    static class Fixture {
+        int x;
+        int y;
+        int expected;
+
+        Fixture(int x, int y, int expected) {
+            this.x = x;
+            this.y = y;
+            this.expected = expected;
+        }
+    }
+}
