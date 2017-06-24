df633716308e8ebf78b812e4cb66875d6a254294
yukung <yukung.i@gmail.com>
2017-06-24 17:43:25 +0900

    8.4 alias と undef

---
+++ fun-ruby/chapter08/alias_undef.rb
@@ -0,0 +1,19 @@
+class C1
+  def hello
+    "Hello"
+  end
+end
+
+class C2 < C1
+  alias old_hello hello
+  
+  def hello
+    "#{old_hello}, again"
+  end
+end
+
+obj = C2.new
+p obj.old_hello
+p obj.hello
+
+# undef はスーパークラスで定義されたメソッドをサブクラスでは削除する、といった用途で使われる
