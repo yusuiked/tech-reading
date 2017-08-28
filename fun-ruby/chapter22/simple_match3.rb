36f1fc4085011880262663cb4a187635215c4b0e
yukung <yukung.i@gmail.com>
2017-08-28 12:40:08 +0900

    22.3.2 前後10文字ずつ表示する

---
+++ fun-ruby/chapter22/simple_match3.rb
@@ -0,0 +1,22 @@
+pattern = Regexp.new("(.{0,10})(" + ARGV[0] + ")(.{0,10})")
+filename = ARGV[1]
+
+count = 0
+File.open(filename) do |file|
+  file.each_line do |line|
+    line.scan(pattern) do |s|
+      prefix_len = 0
+      s[0].each_char do |ch|
+        if ch.ord < 128
+          prefix_len += 1
+        else
+          prefix_len += 2
+        end
+      end
+      space_len = 20 - prefix_len
+      puts "#{" " * space_len}#{s[0]}<<#{s[1]}>>#{s[2]}"
+      count += 1
+    end
+  end
+end
+puts "count: #{count}"
